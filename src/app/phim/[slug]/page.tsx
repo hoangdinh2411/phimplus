163d78545ab451c7afbac4f4dbbd83cee968b275
William Hoang Dinh <hoangdinh2411@gmail.com>
2023-12-04 08:14:51 +0100

    fix error

--- src/app/phim/[slug]/page.tsx
+++ src/app/phim/[slug]/page.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import dynamic from "next/dynamic";
-import Detail from "../../_pages/phim/Detail/Detail";
+import React from 'react';
+import dynamic from 'next/dynamic';
+import Detail from '../../_pages/phim/Detail/Detail';
 // import Trailer from "../../_pages/phim/Trailer/Trailer";
-const Trailer = dynamic(() => import("../../_pages/phim/Trailer/Trailer"), {
+const Trailer = dynamic(() => import('../../_pages/phim/Trailer/Trailer'), {
   ssr: false,
 });
-import Review from "../../../components/shared/Review/Review";
-import MovieContextProvider from "~/provider/MovieContextProvider";
-import { notFound } from "next/navigation";
-import { getMovieBySlug } from "~/services/movieApi";
-import type { Metadata, ResolvingMetadata } from "next";
-import Container from "@mui/material/Container";
+import Review from '../../../components/shared/Review/Review';
+import MovieContextProvider from '~/provider/MovieContextProvider';
+import { notFound } from 'next/navigation';
+import { getMovieBySlug } from '~/services/movieApi';
+import type { Metadata, ResolvingMetadata } from 'next';
+import Container from '@mui/material/Container';
 
 type Props = {
   params: { slug: string };
@@ -22,7 +22,7 @@ export async function generateMetadata(
   const movie = await getMovieBySlug(slug);
   const previousImages = (await parent).openGraph?.images || [];
   if (!movie.status) {
-    return {};
+    return notFound();
   }
   return {
     title: movie.data.seoOnPage?.titleHead || movie.data.titlePage,
@@ -47,7 +47,7 @@ export default async function MovieDetailPage({ params: { slug } }: Props) {
   return (
     <MovieContextProvider movie={movie?.data}>
       <Container
-        component="main"
+        component='main'
         maxWidth={false}
         disableGutters
         sx={{
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,11 @@ import { NextResponse, type NextRequest } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const endPathName = ['phim', 'danh-sach', 'the-loai', 'xem-phim'];
+  const endSlug = request.url.split('/');
 
-  endPathName.forEach((pathName) => {
-    if (request.url.endsWith(pathName)) {
-      return NextResponse.redirect(new URL('/', request.url));
-    }
-  });
+  if (endPathName.includes(endSlug[endSlug.length - 1])) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
 }
 
 // See "Matching Paths" below to learn more
