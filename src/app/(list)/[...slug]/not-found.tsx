fd15b0ad11ff5a1c78632d732c5f48802a305ffb
William Hoang Dinh <hoangdinh2411@gmail.com>
2023-12-05 16:48:33 +0100

    change name for not found file

--- src/app/(list)/[...slug]/not-found.tsx
+++ src/app/(list)/[...slug]/not-found.tsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-export default function NotFoundForFilter() {
+export default function NotFound() {
   return (
     <Container
       maxWidth={false}
