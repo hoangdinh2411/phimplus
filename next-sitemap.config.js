e6c679d5bc42e96f1af77c8d0b575a72cdca4d05
William Hoang Dinh <hoangdinh2411@gmail.com>
2023-12-11 10:47:14 +0100

    adjust domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_DOMAIN || 'https://www.plusphim.com/',
+  siteUrl: process.env.NEXT_PUBLIC_DOMAIN || 'https://www.plusphim.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 
--- src/app/_pages/lien-he/index.tsx
+++ src/app/_pages/lien-he/index.tsx
@@ -1,31 +1,32 @@
-"use client";
-import Box from "@mui/material/Box";
-import { usePathname } from "next/navigation";
-import React from "react";
-import { APP_CONFIG } from "~/helpers/config";
-import { initFacebookSdk } from "~/helpers/loadSDKFacebook";
-import { color } from "~/theme/variables/palette";
+'use client';
+import Box from '@mui/material/Box';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { APP_CONFIG } from '~/helpers/config';
+import { initFacebookSdk } from '~/helpers/loadSDKFacebook';
+import { color } from '~/theme/variables/palette';
 
 export default function CommentsForInFormation() {
   React.useEffect(() => {
     initFacebookSdk();
   });
   const path = usePathname();
+  console.log(path);
 
   return (
     <React.Fragment>
       <Box
-        component="div"
+        component='div'
         py={10}
         sx={{ backgroundColor: color.white, mt: 16 }}
       >
         <div
-          className="fb-comments"
-          data-href={`${APP_CONFIG.DOMAIN}${path.slice(1)}`}
-          data-width="100%"
-          data-numposts="5"
+          className='fb-comments'
+          data-href={`${APP_CONFIG.DOMAIN}${path}`}
+          data-width='100%'
+          data-numposts='5'
           // data-lazy={true}
-          data-colorscheme="light"
+          data-colorscheme='light'
         ></div>
       </Box>
     </React.Fragment>
